0791e7a3950c0a67039f03109074f2a373b0091e
strongdevlancer <jasperkoster687@gmail.com>
2012-11-23 12:28:06 +0530

    Changed the request source header value

--- lib/paypal-sdk/merchant/api.rb
+++ lib/paypal-sdk/merchant/api.rb
@@ -11,7 +11,7 @@ module PayPal
           super("", environment, options)
         end
 
-        MERCHANT_HTTP_HEADER = { "X-PAYPAL-REQUEST-SOURCE" => "merchant-ruby-#{VERSION}" }
+        MERCHANT_HTTP_HEADER = { "X-PAYPAL-REQUEST-SOURCE" => "merchant-ruby-sdk-#{VERSION}" }
         def default_http_header
           super.merge(MERCHANT_HTTP_HEADER)
         end
