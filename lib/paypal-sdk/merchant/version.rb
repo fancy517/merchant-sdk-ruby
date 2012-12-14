8c8008d030c54c6c73e2bc1e25d1ab72a769fed8
strongdevlancer <jasperkoster687@gmail.com>
2012-12-14 17:01:34 +0530

    Update version

--- lib/paypal-sdk/merchant/version.rb
+++ lib/paypal-sdk/merchant/version.rb
@@ -1,7 +1,7 @@
 module PayPal
   module SDK
     module Merchant
-      VERSION = "1.96.0"
+      VERSION = "1.96.1"
     end
   end
 end
--- samples/lib/merchant_samples/version.rb
+++ samples/lib/merchant_samples/version.rb
@@ -1,3 +1,3 @@
 module MerchantSamples
-  VERSION = "1.96.0"
+  VERSION = "1.96.1"
 end
