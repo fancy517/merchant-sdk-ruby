34daf5b8cd5747ed6007610b623a1331ea641f85
strongdevlancer <jasperkoster687@gmail.com>
2014-03-27 13:25:30 -0500

    Added option for Local Shipping and use payment session details

---
+++ merchant-sdk-ruby/samples/lib/merchant_samples.rb
@@ -0,0 +1,10 @@
+require "paypal-sdk-merchant"
+require "merchant_samples/engine"
+require "simple_form"
+require "haml"
+require "twitter-bootstrap-rails"
+require "coderay"
+require "jquery-rails"
+
+module MerchantSamples
+end
---
+++ merchant-sdk-ruby/samples/lib/tasks/merchant_samples_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :merchant_samples do
+#   # Task goes here
+# end
